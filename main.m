92dcf72d873af94a6fc7e6f9ee45ee08eb4b6a94
albertodelacruz96 <albertodelacruz96@gmail.com>
2019-11-11 18:33:54 +0100

    Cambios Hay que poner bien lo de alinear las señales.

--- main.m
+++ main.m
@@ -7,12 +7,12 @@ clc;clear;
 %%%%%%%%%%GROUND TRUTH%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%%Nos devuelve una TABLA no matriz
-M = cleanData('DATASET/anne1.csv');
+M = readtable('DATASET/anne2.csv');
 %Separamos en arrays cada componente t, Ax, Ay, Az
 %Columna 3 --> start Tiempo s?
 %Columna 4 --> Depth mm?
-t_real = M(:, 5).';
-Depth_real = M(:, 3).';
+t_real = ((M.startTime + M.endTime)/2)';
+Depth_real = M.compDepth';
 
 %Pasamos a cm los mm
 Depth_real = Depth_real * 10 ^ -1;
@@ -24,13 +24,13 @@ Depth_real = Depth_real * 10 ^ -1;
 %Columna 2 --> Tiempo (ms)
 %Columna 3,4,5 --> Acceleracion x, y, z (m/s^2)
 
-N = cleanData('DATASET/precalib1_W.csv');
+N = readtable('DATASET/precalib2_W.csv');
 
 %Separamos en arrays cada componente t, Ax, Ay, Az
-t = N(:, 1).';
-Ax = N(:, 2).';
-Ay = N(:, 3).';
-Az = N(:, 4).';
+t = N.Time_Date';
+Ax = N.Acc_X';
+Ay = N.Acc_Y';
+Az = N.Acc_Z';
 
 %Pasamos a segundos los ms
 t = t * 10 ^ -3;
@@ -66,106 +66,66 @@ V_filtrada = V_filtrada * 10 ^ 2;
 %Volvemos a integrar para obtener la distancia
 P = cumtrapz(t, V_filtrada);
 
-%%Obtenemos los picos MAXIMOS y MINIMOS
+%%%QUITAMOS el TRANSITORIO --> Revisar
+%Eliminamos un trozo de la señal?
+P = P(2*Fs:end);
+
+%%Obtenemos los picos MAXIMOS y MINIMOS (Y) y la muestra en la que ocurre (X)
 [picos_max n_max] = findpeaks(P);
 %Genio el de google
 [picos_min n_min] = findpeaks(-P);
 
 %Gestion de EXCEPCIONES
-if length(n_max) > length(n_min)
-    n_max = n_max(1:length(n_min));
-    picos_max = picos_max(1:length(n_min));
-end
-
-if length(n_max) < length(n_min)
-    n_min = n_min(1:length(n_max));
-    picos_min = picos_min(1:length(n_max));
-end
-
-t_teorico = (n_max*1/Fs + n_min*1/Fs)/2;
+%Si los arrays no son iguales, igualar dimensiones
+if length(picos_max) > length(picos_min)
+   picos_min = [picos_min, zeros(1,length(picos_max)-length(picos_min))];
+   n_min = [n_min, zeros(1,length(n_max)-length(n_min))];
+elseif length(picos_min) > length(picos_max)
+   picos_max = [picos_max, zeros(1,length(picos_min)-length(picos_max))];
+   n_max = [n_max, zeros(1,length(n_min)-length(n_max))];
+end 
 
 %Y obtenemos finalmente la PROFUNDIDAD
 Depth_teorica = picos_max + picos_min;
 
-%Encuadrar Ventana (WINDOW) de trabajo
-%%Limite INFERIOR
-for i = 1:length(t_teorico)
-    if t_teorico(1,i) > t_real(1,1)
-        lim_inf = i;
-        break;
-    end
-end
+t_teorico = (n_max*1/Fs + n_min*1/Fs)/2;
 
-%%Limite SUPERIOR
-W = 140;
+%Alinear señal TEORICA y señal REAL
+dif = t_real(1,1) - t_teorico(1,1);
+t_real = t_real - dif;
 
 %Calculamos el error de estimación
-error = Depth_teorica(lim_inf:lim_inf + W - 1) - Depth_real(1:W);
+%error = Depth_teorica(lim_inf:lim_inf + W - 1) - Depth_real(1:W);
 
 %Calculamos el error cuadrático medio
-err = mean((Depth_teorica(lim_inf:lim_inf + W - 1) - Depth_real(1:W)).^2);
-disp(['El error cuadrático medio es: ',num2str(err)])
+%err = mean((Depth_teorica(lim_inf:lim_inf + W - 1) - Depth_real(1:W)).^2);
+%disp(['El error cuadrático medio es: ',num2str(err)])
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%REPRESENTACION DATOS%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%REPRESENTACION GRAFICA%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 figure(1);
-subplot(3,1,1);
-stem(t_teorico(lim_inf:lim_inf + W - 1), Depth_teorica(lim_inf:lim_inf + W - 1), '.');
+subplot(2,1,1);
+stem(t_teorico, Depth_teorica, '.');
 title("Profundidad estimada")
 xlabel("Instante tiempo (s)")
 ylabel("Profundidad (cm)")
-axis([0 max(t_teorico(lim_inf:lim_inf + W - 1)) + 2 0 8])
+axis([0 max(t_teorico) + 2 0 8])
 
-subplot(3,1,2);
-stem(t_real(1:W), Depth_real(1:W), '.');
+subplot(2,1,2);
+stem(t_real, Depth_real, '.');
 title("Profundidad real")
 xlabel("Instante tiempo (s)")
 ylabel("Profundidad (cm)")
-axis([0 max(t_real(1:W)) + 2 0 8])
+axis([0 max(t_real) + 2 0 8])
 
-subplot(3,1,3);
-stem(t_real(1:W), abs(error), '.');
-title("Error")
-xlabel("Compresión")
-ylabel("Error (cm)")
-axis([0 max(t_real(1:W)) + 2 0 8])
-
-function dibujarP1(t, Ax, Ay, Az, A_filtrada, V, P);
-    %Dibujamos cada ACELERACION por separado
-    figure(1)
-
-    subplot(6,1,1);
-    plot(t,Ax);
-    xlabel('Time (s)'); ylabel('Ax (m/s^2)');
-    axis([t(1) t(length(t)) min(Ax) - 1 max(Ax) + 1]);
-
-    subplot(6,1,2);
-    plot(t,Ay);
-    xlabel('Time (s)'); ylabel('Ay (m/s^2)');
-    axis([t(1) t(length(t)) min(Ay) - 1 max(Ay) + 1])
-
-    subplot(6,1,3);
-    plot(t,Az);
-    xlabel('Time (s)'); ylabel('Az (m/s^2)');
-    axis([t(1) t(length(t)) min(Az) - 1 max(Az) + 1])
-
-    subplot(6,1,4);
-    plot(t,A_filtrada);
-    xlabel('Time (s)'); ylabel('Acceleration (m/s^2)');
-    axis([t(1) t(length(t)) min(A_filtrada) - 1 max(A_filtrada) + 1])
-
-    subplot(6,1,5)
-    plot(t,V);
-    xlabel('Time (s)'); ylabel('Velocity (cm/s)');
-    axis([t(1) t(length(t)) min(V) - 0.1 max(V) + 0.1])
-
-    subplot(6,1,6)
-    plot(t,P);
-    xlabel('Time (s)'); ylabel('Distance (cm)');
-    axis([t(1) t(length(t)) min(P) - 1 max(P) + 1])
-end
+%subplot(3,1,3);
+%stem(t_real, abs(error), '.');
+%title("Error")
+%xlabel("Compresión")
+%ylabel("Error (cm)")
+%axis([0 max(t_real) + 2 0 8])
 
 function [P,f]= analisis_frecuencial(senal, Fs)
     %Respuesta en frecuencia
@@ -194,11 +154,5 @@ function senal_salida = filtrado_senal(senal_entrada, f_filtrado, orden, Fs, tip
     end
     
     senal_salida = filtfilt(b,a,senal_entrada);
-end
-
-function x = cleanData(nombre_fich)
-    M = readtable(nombre_fich);
-    N = table2array(M(:,2:width(M)));
-    x = N;
 end
 
