22863502f6550b971384a923a1d31082650454b5
Eder <ederdelblanco@gmail.com>
2019-12-21 19:58:36 +0100

    Añadidos percentiles 50 y 10 a Bland-Altman
    Ahora se representan las líneas correspondientes a los percentiles 50 y 10 del valor absoluto del error

--- bland_altman.m
+++ bland_altman.m
@@ -5,6 +5,8 @@ load user_stats.mat
 y = [user_stats.depth_abs_error];
 x = [user_stats.comp_depth_real];
 perc_90 = prctile(abs(y),90);
+perc_50 = prctile(abs(y),50);
+perc_10 = prctile(abs(y),10);
 
 close all
 figure(1)
@@ -12,26 +14,36 @@ subplot(2,1,1)
 plot(x,y,'.')
 hold on
 plot([2 7],[0 0],'--r')
-plot([2 7],[1 1]*perc_90,'Color',[0.5 0.5 0.5],'LineStyle','-.')
-plot([2 7],[1 1]*-perc_90,'Color',[0.5 0.5 0.5],'LineStyle','-.')
+plot([2 7],[1 1]*perc_90,'Color',[1 1 1]*0.67,'LineStyle','-.')
+plot([2 7],[1 1]*-perc_90,'Color',[1 1 1]*0.67,'LineStyle','-.')
+plot([2 7],[1 1]*perc_50,'Color',[1 1 1]*0.5,'LineStyle','-.')
+plot([2 7],[1 1]*-perc_50,'Color',[1 1 1]*0.5,'LineStyle','-.')
+plot([2 7],[1 1]*perc_10,'Color',[1 1 1]*0.33,'LineStyle','-.')
+plot([2 7],[1 1]*-perc_10,'Color',[1 1 1]*0.33,'LineStyle','-.')
 title('Bland-Altman para el error de profundidad')
 xlabel('Profundidad de compresión real (cm)')
 ylabel('Error (cm)')
-legend({'Error','Cero','Percentil 90'})
+legend({'Error','Cero','Percentil 90','Percentil 50','Percentil 10'})
 
 y = [user_stats.rate_abs_error];
 x = [user_stats.comp_rate_real];
 perc_90 = prctile(abs(y),90);
+perc_50 = prctile(abs(y),50);
+perc_10 = prctile(abs(y),10);
 
 subplot(2,1,2)
 plot(x,y,'.')
 hold on
 plot([60 140],[0 0],'--r')
-plot([60 140],[1 1]*perc_90,'Color',[0.5 0.5 0.5],'LineStyle','-.')
-plot([60 140],[1 1]*-perc_90,'Color',[0.5 0.5 0.5],'LineStyle','-.')
+plot([60 140],[1 1]*perc_90,'Color',[1 1 1]*0.67,'LineStyle','-.')
+plot([60 140],[1 1]*-perc_90,'Color',[1 1 1]*0.67,'LineStyle','-.')
+plot([60 140],[1 1]*perc_50,'Color',[1 1 1]*0.5,'LineStyle','-.')
+plot([60 140],[1 1]*-perc_50,'Color',[1 1 1]*0.5,'LineStyle','-.')
+plot([60 140],[1 1]*perc_10,'Color',[1 1 1]*0.33,'LineStyle','-.')
+plot([60 140],[1 1]*-perc_10,'Color',[1 1 1]*0.33,'LineStyle','-.')
 title('Bland-Altman para el error de frecuencia')
 xlabel('Frecuencia de compresión real (cpm)')
 ylabel('Error (cpm)')
-legend({'Error','Cero','Percentil 90'})
+legend({'Error','Cero','Percentil 90','Percentil 50','Percentil 10'})
 
 end
